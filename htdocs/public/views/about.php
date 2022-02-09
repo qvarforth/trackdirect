e8c7a8c34dd827584043b358f8889771a2a91dda
Peter Buchegger <peterus@users.noreply.github.com>
2022-02-10 00:58:53 +0100

    Update about.php

--- htdocs/public/views/about.php
+++ htdocs/public/views/about.php
@@ -1,4 +1,4 @@
-<?php require "${_SERVER["DOCUMENT_ROOT"]}/includes/bootstrap.php"; ?>
+<?php require "${_SERVER["DOCUMENT_ROOT"]}/../includes/bootstrap.php"; ?>
 
 <title>About / FAQ</title>
 <div class="modal-inner-content modal-inner-content-about" style="padding-bottom: 30px;">
